2de14e89ed7737314008deca460d47f77c9222bf
Denizhan Şahin <denizhansahin@yaani.com>
2024-11-14 14:47:55 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,27 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { EmployeesService } from './employees/employees.service';
+import {ThrottlerModule, ThrottlerGuard} from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule,
+    ThrottlerModule.forRoot([
+      { name:'short',
+        ttl:60000,
+      limit:3},
+      { name:'long',
+        ttl:60000,
+      limit:100}
+    ]),
+    MyLoggerModule
+  ],
   controllers: [AppController],
-  providers: [AppService,EmployeesService],
+  providers: [AppService,EmployeesService,
+    {provide:APP_GUARD,
+      useClass:ThrottlerGuard,
+    }
+  ],
 })
 export class AppModule {}
--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,23 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Ip } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
+import { Role } from 'prisma/role.enum';
 
 
-
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name)
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
-
+  
+  @SkipThrottle({default:false})
   @Get()
-  findAll(@Query('role') role? : 'INTERN' | 'ENGINEER' | 'ADMIN') {
+  findAll(@Ip() ip:string,@Query('role') role? : Role) {
+    this.logger.log(`Request for All employees \t${ip}`)
     return this.employeesService.findAll(role);
   }
 
+
+  @Throttle({short : { ttl:100, limit:1}})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
